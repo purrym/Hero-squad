31f36389d5c8ad52c05c45c22f87c790e7c7f234
purrym <wanjirapurity@gmail.com>
2023-06-07 12:14:37 +0300

    updated database

---
+++ src/main/java/org/practice/db/database.java
@@ -0,0 +1,20 @@
+package org.practice.db;
+
+
+
+import org.sql2o.Sql2o;
+
+
+
+
+public class database {
+    //DATABASE CONNECTION
+    private static final Sql2o connection = new Sql2o (
+
+            "jdbc:postgresql://localhost:5432/hero_squad",
+            "postgres",
+            "Goddidit"
+    );
+    public static Sql2o getConnect() { return connection; }
+}
+
